0280c97726d980507d2a5bca65f522610b1327e2
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-05-17 23:48:47 -0300

    Start marketing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FirstStepComponent } from './first-step/first-step.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BeneficiosComponent } from './tabs/beneficios/beneficios.component';
+import { SobreComponent } from './tabs/sobre/sobre.component';
+import { MatButtonModule } from '@angular/material/button';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstStepComponent,
+    BeneficiosComponent,
+    SobreComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
